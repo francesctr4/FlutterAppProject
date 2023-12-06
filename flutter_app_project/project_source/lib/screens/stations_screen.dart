50fa56b0ab2dd0d857fb074049662949e380c333
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2023-12-06 19:21:57 +0100

    FutureBuilder in Stations Screen + StationListItem In Progress

--- flutter_app_project/project_source/lib/screens/stations_screen.dart
+++ flutter_app_project/project_source/lib/screens/stations_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:project_source/api/line_stations_api.dart';
 import 'package:project_source/models/line_class.dart';
+import 'package:project_source/models/station_class.dart';
 import 'package:project_source/widgets/bottom_bar_widget.dart';
+
+import '../widgets/station_list_item.dart';
 
 class StationsScreen extends StatelessWidget {
   const StationsScreen({super.key});
@@ -45,6 +49,31 @@ class _StationsScreenImplementationState
       backgroundColor: Colors.white,
       body: Stack(
         children: [
+          FutureBuilder(
+            future: apiLoadStationsFromLine(widget.line),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Station>> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final stationList = snapshot.data!;
+              return ListView.separated(
+                itemCount: stationList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  stationList.sort(
+                      (a, b) => a.stationOrder!.compareTo(b.stationOrder!));
+                  return StationListItem(station: stationList[index]);
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider(
+                    height: 1,
+                  );
+                },
+              );
+            },
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: BottomBarWidget(),
---
+++ flutter_app_project/project_source/lib/widgets/station_list_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:project_source/models/station_class.dart';
+
+class StationListItem extends StatelessWidget {
+  const StationListItem({
+    super.key,
+    required this.station,
+  });
+
+  final Station station;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
