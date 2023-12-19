c7c16dbb8bed534d481178dec290d1c6b9aeda46
Mario Garcia Sutil <mariosutil2003@gmail.com>
2023-12-19 19:39:49 +0100

    Home Screen visuals finished (missing button logic)

--- flutter_app_project/project_source/lib/screens/home_screen.dart
+++ flutter_app_project/project_source/lib/screens/home_screen.dart
@@ -7,116 +7,214 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 650,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    width: screenSize.width,
-                    height: 400,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        height: screenSize.height,
+        color: const Color.fromRGBO(226, 238, 252, 1),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 500,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      width: screenSize.width,
+                      height: 400,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Image.asset(
+                        'resources/Home_Screen_Train.png',
+                        fit: BoxFit.fitWidth,
+                      ),
                     ),
-                    child: Image.asset(
-                      'resources/Home_Screen_Train.png',
-                      fit: BoxFit.fitWidth,
+                  ),
+                  Align(
+                    alignment: const Alignment(0, 1),
+                    child: Container(
+                      width: screenSize.width - 50,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        gradient: const LinearGradient(
+                          begin: Alignment(0.0, 1.0),
+                          end: Alignment(1.0, 0.0),
+                          colors: <Color>[
+                            Color.fromRGBO(64, 70, 136, 1),
+                            Color.fromRGBO(95, 109, 255, 1),
+                          ],
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Container(
+                              width: screenSize.width - 100,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(15),
+                                    topRight: Radius.circular(15)),
+                              ),
+                              child: const Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'Origin',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: screenSize.width - 100,
+                            height: 1,
+                            color: Colors.grey,
+                          ),
+                          Container(
+                            width: screenSize.width - 100,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(15),
+                                  bottomRight: Radius.circular(15)),
+                            ),
+                            child: const Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Destination',
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 18, left: 26, right: 26),
+                            child: SizedBox(
+                              width: screenSize.width,
+                              height: 36,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 32, 40, 127),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Search Combination',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 40, left: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Favorite Routes',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Container(
-                  width: screenSize.width - 50,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    gradient: const LinearGradient(
-                      begin: Alignment(0.0, 1.0),
-                      end: Alignment(1.0, 0.0),
-                      colors: <Color>[
-                        Color.fromRGBO(64, 70, 136, 1),
-                        Color.fromRGBO(95, 109, 255, 1),
-                      ],
-                    ),
-                  ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+              child: Container(
+                width: screenSize.width,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Container(
-                          width: screenSize.width - 100,
-                          height: 50,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(15),
-                                topRight: Radius.circular(15)),
-                          ),
-                          child: const Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              'Origin',
-                            ),
-                          ),
+                      Text(
+                        'You do not have favorite routes.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
                       ),
-                      Container(
-                        width: screenSize.width - 100,
-                        height: 1,
-                        color: Colors.grey,
-                      ),
-                      Container(
-                        width: screenSize.width - 100,
-                        height: 50,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(15),
-                              bottomRight: Radius.circular(15)),
-                        ),
-                        child: const Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Destination',
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 18, left: 26, right: 26),
-                        child: SizedBox(
-                          width: screenSize.width,
-                          height: 36,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color.fromARGB(255, 32, 40, 127),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                              ),
-                            ),
-                            child: const Text(
-                              'Search Combination',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
+                      Text(
+                        'When you do a search you will be able to add it as a favorite and then you will see it here.',
+                        style: TextStyle(
+                          fontSize: 15,
                         ),
                       ),
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.only(top: 20, left: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Favorite Stations',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+              child: Container(
+                width: screenSize.width,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'You do not have favorite stations.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'Add a station to your list to view upcoming trains.',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
