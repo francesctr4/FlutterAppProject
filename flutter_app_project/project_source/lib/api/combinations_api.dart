c7258c8dccca71b8ed2e3899dc0f14184682565b
Mario Garcia Sutil <mariosutil2003@gmail.com>
2023-12-20 13:33:37 +0100

    combinations class rename

--- flutter_app_project/project_source/lib/api/combinations_api.dart
+++ flutter_app_project/project_source/lib/api/combinations_api.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 
-import 'package:project_source/models/all_stations_class.dart';
+import 'package:project_source/models/combinations_class.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<AllStation>> apiLoadLines()async{
-  final uri = Uri.parse("https://api.tmb.cat/v1/transit/linies/metro?app_id=9a0ad75d&app_key=286c514e860aba9c094078dcf4a5d4c4");
+  final uri = Uri.parse("https://api.tmb.cat/v1/transit/estacions?app_id=9a0ad75d&app_key=286c514e860aba9c094078dcf4a5d4c4");
   final response = await http.get(uri);
   final json = jsonDecode(response.body);
   final jsonStationsList = json["features"];
